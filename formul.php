2228c3c2705fbd496f5b972c2137449c9c769d95
fatima zahra <fma87879@gmail.com>
2024-03-31 00:55:34 +0000

    tp

--- formul.php
+++ formul.php
@@ -2,14 +2,31 @@
 <?php
 $conn = new PDO("mysql:host=localhost; dbname=something1", "root", "");
 
-$id=$_GET["id"];
-$query = $conn -> prepare("SELECT * FROM employees WHERE employeeNumber= :id");
-$query -> bindParam(":id" , $id);
-$query -> execute();
-$row = $query->fetch(PDO::FETCH_ASSOC);
+// $id=$_GET["id"];
+// $query = $conn -> prepare("SELECT * FROM employees WHERE employeeNumber= :id");
+// $query -> bindParam(":id" , $id);
+// $query -> execute();
+// $row = $query->fetch(PDO::FETCH_ASSOC);
 
 
-echo $row["firstName"] . " " . $row["lastName"] . " " . $row["email"];
+// echo $row["firstName"] . " " . $row["lastName"] . " " . $row["email"];
+
+
+
+
+if (isset($_GET["modify"]) && $_GET["modify"] == "modify-employee" && isset($_GET["id"])) {
+    $id=$_GET["id"];
+    $lastname = $_GET["lastname"];
+    $firstname = $_GET["firstname"];
+    $email = $_GET["eml"];
+    $query = $conn -> prepare("UPDATE employees SET lastName= :lastname , firstName= :firstname, email= :email WHERE employeeNumber= :id");
+    $query -> bindParam(":id" , $id);
+    $query -> bindParam(":lastname" , $lastname);
+    $query -> bindParam(":firstname" , $firstname);
+    $query -> bindParam(":email" , $email);
+    $query -> execute();
+}
+
 // if (isset($_GET["modify"]) && $_GET["modify"] == "modify-employee" && isset($_GET["id"])) {
 //     $lastname = $_GET["lastname"];
 //     $firstname = $_GET["firstname"];
@@ -18,7 +35,7 @@ echo $row["firstName"] . " " . $row["lastName"] . " " . $row["email"];
 //     $query -> execute([ $lastname, $firstname, $email, $_GET["id"]]);
 // }
 ?>
-    <!-- <form action="" method="GET">
+    <form action="" method="GET">
         <label for="fn">first name</label>
         <input type="text" id="fn" name="firstname"><br>
         <label for="ln">last name</label>
@@ -26,4 +43,4 @@ echo $row["firstName"] . " " . $row["lastName"] . " " . $row["email"];
         <label for="em">email</label>
         <input type="email" name="eml" id="em">
         <button type="submit" name="modify" value="modify-employee">submit</button>
-    </form> -->
+    </form>
